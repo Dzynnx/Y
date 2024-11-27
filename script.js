75e3d1334ca91aa4de89b5f9533fe41413c5c367
Dzynnx <896146587@qq.com>
2024-11-28 03:52:38 +0800

    添加新内容

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const music = document.getElementById('background-music');
-    const playMusicBtn = document.getElementById('play-music-btn');
+  const music = document.getElementById('background-music');
+  const playButton = document.getElementById('enterButton');
   
-    playMusicBtn.addEventListener('click', () => {
-      if (music.paused) {
-        music.play();
-        playMusicBtn.textContent = '暂停音乐';
-      } else {
-        music.pause();
-        playMusicBtn.textContent = '播放音乐';
-      }
-    });
+  // 判断本地存储是否有播放状态，若有，直接播放音乐
+  if(localStorage.getItem('isMusicPlaying') === 'true') {
+    music.play();
+  }
+
+  playButton.addEventListener('click', () => {
+    // 播放背景音乐
+    music.play();
+    // 标记背景音乐已播放
+    localStorage.setItem('isMusicPlaying', 'true');
+    
+    // 给音乐播放几秒钟后再跳转到第二个界面
+    setTimeout(() => {
+      // 跳转到第二个页面
+      window.location.href = 'page2.html';
+    }, 2000); // 延迟2秒后跳转
   });
-  
+});
